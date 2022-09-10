3674fa107e223f5d6813b0e8af184658f55151f6
Navpreet Singh Sidhu <73638005+nsidhu11@users.noreply.github.com>
2022-09-09 17:22:41 -0700

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,66 @@
+class Controls{
+
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
+        // # is a private method
+    }
+
+    #addKeyboardListeners(){
+
+        // (event)=> 
+        // using arrow treats (this) as a same this thing in both 
+        // function and class
+
+        // whithout =>
+        // document.onkeydown=function(event)
+        // (this) used in the function will be different from constructor
+
+
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
+                    break;
+                case "ArrowRight":
+                    this.right=true;
+                    break;
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
+                    break;
+            }
+        }
+
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":   
+                    this.reverse=false;
+                    break;
+            }
+
+        }
+    }
+}
